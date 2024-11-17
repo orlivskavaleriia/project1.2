fff15686e83c677cbbbcbb5c7530ce1c8072ec1b
orlivskavaleriia <orlivskavaleriia@gmail.com>
2024-11-17 23:05:18 +0200

    del file

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Перезапускаємо Docker контейнери за допомогою Docker Compose
-
-cd project1
-
-# Оновлюємо контейнери, зупиняємо старі та запускаємо нові
-docker-compose down
-docker-compose up -d --build
